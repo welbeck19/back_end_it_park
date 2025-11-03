e792cb7de2f3b66aedbddf498e1b32959e9b8e84
welbeck19 <elbekoch19@gmail.com>
2025-11-03 15:28:18 +0500

    task 9

--- temp.py
+++ temp.py
@@ -1,3 +1,6 @@
-#Task 8
-fruits = ('apple', 'banana', 'cherry')
-print('banana' in fruits)
+#Task 9
+person = ("Ali", 25, "Toshkent")
+(item1, item2, item3) = person
+print(item1)
+print(item2)
+print(item3)
--- tuples_lesson.py
+++ tuples_lesson.py
@@ -33,3 +33,10 @@ print(len(digits))
 #Task 8
 fruits = ('apple', 'banana', 'cherry')
 print('banana' in fruits)
+
+#Task 9
+person = ("Ali", 25, "Toshkent")
+(item1, item2, item3) = person
+print(item1)
+print(item2)
+print(item3)
