f4a944c346b544955a6aaffa9c217df4ef508e59
welbeck19 <elbekoch19@gmail.com>
2025-11-05 10:41:06 +0500

    task 12

--- if_elif_lesson.py
+++ if_elif_lesson.py
@@ -88,3 +88,14 @@ elif ("python" in skills) ^ ("sql" in skills):
     print("Bittasi bor")
 else:
     print("Ikkalasi ham yo'q")
+
+
+#Task 12
+while True:
+    user_password = input("Please type your password: ")
+    if len(user_password) > 10:
+        print("Strong password!")
+    elif 6 <= len(user_password) <= 10:
+        print("Average password!")
+    elif len(user_password) < 6:
+        print("Too short password!")
