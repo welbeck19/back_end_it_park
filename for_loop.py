1b6a6717d5ca1769076c56275a398b6f3dca791b
welbeck19 <elbekoch19@gmail.com>
2025-11-05 15:31:04 +0500

    task 6

--- for_loop.py
+++ for_loop.py
@@ -49,3 +49,14 @@ for i in words:
     for letter in letters:
         new_word = letters[0] + letters[-1]
     print(new_word)
+
+#Task 6
+nums = [5, 9, 15, 20, 22, 30, 35]
+result = []
+
+for num in nums:
+    if num % 3 == 0 and num % 5 == 0:
+        result.append(num)
+
+average = sum(result) / len(result)
+print(average)
