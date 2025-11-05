d5b55d1708c2b990bb45d5e2c86c2c105f65974a
welbeck19 <elbekoch19@gmail.com>
2025-11-05 10:15:58 +0500

    task 9

--- if_elif_lesson.py
+++ if_elif_lesson.py
@@ -58,3 +58,13 @@ if profile["age"] >= 18:
     print("Voyaga yetgan")
 else:
     print("Voyaga yetmagan")
+
+#Task 9
+is_logged_in = True
+is_admin = False
+if is_logged_in and is_admin:
+    print("Admin panelga xush kelibsiz")
+elif is_logged_in:
+    print("Foydalanuvchi paneli")
+else:
+    print("Kirish talab qilinadi")
--- temp.py
+++ temp.py
@@ -1,6 +1,9 @@
-#Task 8
-profile = {"name": "Alim", "age": 15}
-if profile["age"] >= 18:
-    print("Voyaga yetgan")
+#Task 9
+is_logged_in = True
+is_admin = False
+if is_logged_in and is_admin:
+    print("Admin panelga xush kelibsiz")
+elif is_logged_in:
+    print("Foydalanuvchi paneli")
 else:
-    print("Voyaga yetmagan")
+    print("Kirish talab qilinadi")
