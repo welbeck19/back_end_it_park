254724a393d18f49bcd63173f7c91b74884f8d02
welbeck19 <elbekoch19@gmail.com>
2025-11-05 15:14:14 +0500

    task 3

--- for_loop.py
+++ for_loop.py
@@ -20,3 +20,16 @@ for i in words:
     if count >= 2:
         print(f"Number of vowels in {i}: {count}")
 
+#Task 3
+numbers = [7, 12, -3, 8, 0, 15]
+max = float('-inf')
+min = float('inf')
+for i in numbers:
+    if i > max:
+        max = i
+print(f"Max value = {max}")
+for i in numbers:
+    if i < min:
+        min = i
+print(f"Min value = {min}")
+print(f"Max - Min = {max - min}")
--- temp.py
+++ temp.py
@@ -1,14 +1,13 @@
-#Task 2
-words = ["apple", "sky", "queue", "book", "cry"]
-for i in words:
-    count = 0
-    word = list(i)
-    for letter in word:
-        vowels = ["a", "e", "o", "u", "i"]
-        if letter in vowels:
-            count += 1
-        else:
-            continue
-    if count >= 2:
-        print(f"Number of vowels in {i}: {count}")
-
+#Task 3
+numbers = [7, 12, -3, 8, 0, 15]
+max = float('-inf')
+min = float('inf')
+for i in numbers:
+    if i > max:
+        max = i
+print(f"Max value = {max}")
+for i in numbers:
+    if i < min:
+        min = i
+print(f"Min value = {min}")
+print(f"Max - Min = {max - min}")
