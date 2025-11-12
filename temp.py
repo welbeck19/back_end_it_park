8992a8bd2a8f58df3040a794b33eff00efb60da1
welbeck19 <elbekoch19@gmail.com>
2025-11-12 15:26:03 +0500

    task 7

--- temp.py
+++ temp.py
@@ -1,12 +1,1 @@
-#Task 6
-def input_password(password):
-    
-    if not password:
-        raise ValueError("Empty password!")
-    
-password = input("Type your password: ")
-
-try:
-    input_password(password)
-except Exception as e:
-    print(e)
+#Task 7
