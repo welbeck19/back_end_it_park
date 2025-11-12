1a7b5e00c3873af58a4d04e31e8d9e1c5cd93e7f
welbeck19 <elbekoch19@gmail.com>
2025-11-12 16:02:38 +0500

    task 9

--- temp.py
+++ temp.py
@@ -1,12 +1,1 @@
-#Task 8
-def make_integer(number):
-    
-    print(number)
-    
-number = int(input("Type any number: "))
-
-try:
-    make_integer(number)
-
-finally:
-    print("End!")
+#Task 9
