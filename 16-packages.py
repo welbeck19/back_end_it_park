4c688a36f228ea5236672b915d6b813416e4a5f3
welbeck19 <elbekoch19@gmail.com>
2025-11-17 15:22:20 +0500

    Task 2

--- 16-packages.py
+++ 16-packages.py
@@ -1,5 +1,5 @@
-import requests
-
+#16-packages
+#Task 1
 url = "https://jsonplaceholder.typicode.com/posts"
 response = requests.get(url)
 
@@ -12,3 +12,26 @@ if response.status_code == 200:
     print(data[4]) 
 else:
     print("Xatolik yuz berdi.")
+
+#Task 2
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.google.com"
+response = requests.get(url)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = soup.find_all('a')
+
+
+    print("Found links: ")
+    for link in links:
+        href = link.get('href')
+        if href:
+            print(href)
+        else:
+            print("No href attribute")
+else:
+    print(f"Failed to retrieve the page. Statues code: {response.status_code}")
+
+#Task 3
